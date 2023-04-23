6fa479643894bcfc1f41e1782334934677f044ae
monika9999 <sowka16@gmail.com>
2023-04-23 20:54:17 +0200

    3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let changeColor = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startButton.addEventListener('click', () => {
+  if (
+    (changeColor = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000))
+  ) {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+  }
+});
+
+stopButton.addEventListener('click', () => {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  clearInterval(changeColor);
+});
 
